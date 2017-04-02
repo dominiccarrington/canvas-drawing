7e80597f46b8aeb23af05f6f1e0532ecbc344275
Dominic Carrington <carringtondominic@gmail.com>
2017-04-02 17:43:00 +0100

    Initial commit with canvas classes

---
+++ base/Dom.ts
@@ -0,0 +1,22 @@
+class Dom
+{
+    public static createCheckbox(label?: string): HTMLInputElement
+    {
+        let container = this.createElement("div");
+        if (label) {
+            container = this.createElement("label", container);
+            container.textContent = label;
+        }
+        let element = this.createElement("input", container) as HTMLInputElement;
+        element.type = "checkbox";
+        
+        return element;
+    }
+
+    public static createElement(tag: string, parent = document.body): HTMLElement
+    {
+        let element = document.createElement(tag);
+        parent.appendChild(element);
+        return element;
+    }
+}
