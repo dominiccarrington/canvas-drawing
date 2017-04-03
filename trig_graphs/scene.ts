cdd1f5586b094029ff9b83411539726fe3c0e72e
Dominic Carrington <carringtondominic@gmail.com>
2017-04-03 14:30:14 +0100

    Added basic transformations to trig graphs

--- trig_graphs/scene.ts
+++ trig_graphs/scene.ts
@@ -2,7 +2,7 @@
 /// <reference path="../base/Dom.ts" />
 
 const maxValue = TWO_PI * 2;
-const step = 0.01;
+const step = 0.005;
 let c: Canvas;
 let sinCheckbox: HTMLInputElement;
 let cosCheckbox: HTMLInputElement;
@@ -16,6 +16,21 @@ function setup()
 
     cosCheckbox = Dom.createCheckbox("Cos Graph");
     tanCheckbox = Dom.createCheckbox("Tan Graph");
+
+    let span = Dom.createSpan("y = ");
+    let vStretchNum = Dom.createTextbox(span);
+    vStretchNum.style.width = "25px";
+    vStretchNum.id = "vStretch";
+    vStretchNum.setAttribute("value", "1");
+
+    span.innerHTML += " f( ";
+
+    let hStretchNum = Dom.createTextbox(span);
+    hStretchNum.style.width = "25px";
+    hStretchNum.id = "hStretch";
+    hStretchNum.setAttribute("value", "1");
+
+    span.innerHTML += " x)";
 }
 
 function draw()
@@ -25,12 +40,15 @@ function draw()
     c.stroke(0);
 
     c.line(0, 150, c.width, 150);
-    for (let i = 0; i <= c.width; i += 90) {
+    c.line(0, 0, 0, c.height);
+    for (let i = 90; i <= c.width; i += 90) {
         c.dashLine(i, 0, i, c.height, [5, 5, 5]);
     }
 
     let prevX;
     let prevY;
+    let vStretch = parseInt((<HTMLInputElement> Dom.get("#vStretch")).value);
+    let hStretch = parseInt((<HTMLInputElement> Dom.get("#hStretch")).value);
     
     c.font("18px Courier New");
     c.strokeWidth(2);
@@ -38,15 +56,15 @@ function draw()
         prevX = 0;
         prevY = 150;
         c.stroke(255, 0, 0);
+
         for (let i = -maxValue; i <= maxValue; i += step) {
             let x = CanvasMath.degrees(i); // RAD -> DEG
-            let y = CanvasMath.map(Math.sin(i), -1, 1, 290, 10);
+            let y = CanvasMath.map(vStretch * Math.sin(hStretch * i), -1, 1, 290, 10);
 
             c.line(prevX, prevY, x, y);
             prevX = x;
             prevY = y;
         }
-
     }
 
     if (cosCheckbox.checked) {
@@ -55,7 +73,7 @@ function draw()
         prevY = 10;
         for (let i = -maxValue; i <= maxValue; i += step) {
             let x = CanvasMath.degrees(i); // RAD -> DEG
-            let y = CanvasMath.map(Math.cos(i), -1, 1, 290, 10);
+            let y = CanvasMath.map(vStretch * Math.cos(hStretch * i), -1, 1, 290, 10);
 
             c.line(prevX, prevY, x, y);
             prevX = x;
@@ -69,9 +87,9 @@ function draw()
         prevY = 150;
         for (let i = -maxValue; i <= maxValue; i += step) {
             let x = CanvasMath.degrees(i);
-            let y = CanvasMath.map(Math.tan(i), -1, 1, 290, 10);
+            let y = CanvasMath.map(vStretch * Math.tan(hStretch * i), -1, 1, 290, 10);
 
-            for (let i = 90; i <= c.width; i += 180) {
+            for (let i = 90 / hStretch; i <= c.width; i += 180 / hStretch) {
                 if (prevX < i && x > i) {
                     prevX = x;
                     prevY = y;
@@ -92,40 +110,45 @@ function draw()
     let yVal = 18;
 
     if (sinCheckbox.checked) {
-        let value = CanvasMath.round(Math.sin(CanvasMath.radians(c.events.mouseX)), 5);
+        let value = CanvasMath.round(vStretch * Math.sin(hStretch * CanvasMath.radians(c.events.mouseX)), 5);
         if (value < 0.0005 && value > -0.0005) {
             value = 0;
         }
-        let text = "sin(" + c.events.mouseX + ") = " + value;
+        // let text = "sin(" + c.events.mouseX + ") = " + value;
+        let text = (vStretch != 1 ? vStretch : "") + "sin(" + (hStretch != 1 ? hStretch + "*" : "") + c.events.mouseX + ") = " + value;
+
         c.text(c.width - c.textSize(text) - 5, yVal, text);
         yVal += 20;
     }
 
     if (cosCheckbox.checked) {
-        let value = CanvasMath.round(Math.cos(CanvasMath.radians(c.events.mouseX)), 5);
+        let value = CanvasMath.round(vStretch * Math.cos(hStretch * CanvasMath.radians(c.events.mouseX)), 5);
         if (value < 0.0005 && value > -0.0005) {
             value = 0;
         }
-        let text = "cos(" + c.events.mouseX + ") = " + value;
+        // let text = "cos(" + c.events.mouseX + ") = " + value;
+        let text = (vStretch != 1 ? vStretch : "") + "cos(" + (hStretch != 1 ? hStretch + "*" : "") + c.events.mouseX + ") = " + value;
+
         c.text(c.width - c.textSize(text) - 5, yVal, text);
         yVal += 20;
     }
 
     if (tanCheckbox.checked) {
-        let value = CanvasMath.round(Math.tan(CanvasMath.radians(c.events.mouseX)), 5);
+        let value = CanvasMath.round(vStretch * Math.tan(hStretch * CanvasMath.radians(c.events.mouseX)), 5);
         let infStretch = false;
         if (value < 0.0005 && value > -0.0005) {
             value = 0;
         }
 
-        for (let i = 90; i <= c.width; i += 180) {
+        for (let i = 90 / hStretch; i <= c.width; i += 180 / hStretch) {
             if (c.events.mouseX == i) {
                 infStretch = true;
-                break;
             }
         }
 
-        let text = "tan(" + c.events.mouseX + ") = " + (!infStretch ? value : "∞");
+        // let text = "tan(" + c.events.mouseX + ") = " + value;
+        let text = (vStretch != 1 ? vStretch : "") + "tan(" + (hStretch != 1 ? hStretch + "*" : "") + c.events.mouseX + ") = " + (infStretch ? "∞" : value);
+
         c.text(c.width - c.textSize(text) - 5, yVal, text);
         yVal += 20;
     }
